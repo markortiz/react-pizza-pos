0424e2eb3c7280519113bb171f1acedeb18cfdd4
makus <markkennethortiz@gmail.com>
2020-05-02 00:29:16 +0800

    chore: move app to views directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const PizzaApp = React.lazy(() => import('./views/PizzaApp/PizzaApp'));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PizzaApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
