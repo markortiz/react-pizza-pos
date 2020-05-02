dd6caf6200a4e57354a0675d3b474b4776bcc4b2
makus <markkennethortiz@gmail.com>
2020-05-02 21:42:04 +0800

    chore: add crust icon

--- src/components/OrderPage/PizzaCrust/Crust.tsx
+++ src/components/OrderPage/PizzaCrust/Crust.tsx
@@ -3,17 +3,20 @@ import './Crust.css'
 import { ReactComponent as PizzaThinIcon } from '../../../assets/img/pizza-thin.svg'
 import { ReactComponent as PizzaThickIcon } from '../../../assets/img/pizza-thick.svg'
 
-function PizzaCrust() {
+function PizzaCrust(props: any) {
+
+  const { onNextView } = props;
+
   return (
-    <section className="PizzaCrust animated fadeIn">
+    <section className="PizzaCrust">
       <h2 className="PizzaCrust-title">Select your pizza crust:</h2>
       <div className="container">
         <div className="PizzaCrust-container row">
-          <button className="PizzaCrust-icon col-5">
+          <button className="PizzaCrust-icon col-5" onClick={onNextView}>
             <PizzaThinIcon className="pizza-icon col-12"/>
             <span>Thin - (+$2)</span>
           </button>
-          <button className="PizzaCrust-icon col-5">
+          <button className="PizzaCrust-icon col-5" onClick={onNextView}>
             <PizzaThickIcon className="pizza-icon col-12"/>
             <span>Thick - (+$4)</span>
           </button>
