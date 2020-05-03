4bf003ef0a7ae112d624215b83ad218f1e1ec296
makus <markkennethortiz@gmail.com>
2020-05-03 15:39:38 +0800

    chore: add crust to cart

--- src/components/OrderPage/PizzaCrust/Crust.tsx
+++ src/components/OrderPage/PizzaCrust/Crust.tsx
@@ -3,23 +3,36 @@ import './Crust.css'
 import { ReactComponent as PizzaThinIcon } from '../../../assets/img/pizza-thin.svg'
 import { ReactComponent as PizzaThickIcon } from '../../../assets/img/pizza-thick.svg'
 
-function PizzaCrust(props: any) {
+interface Crust {
+  name: string;
+  price: number;
+}
 
-  const { onNextView } = props;
+function PizzaCrust(props: any) {
+  const { onNextView, crusts } = props;
+  const setCrust = (crust: Crust) => {
+    onNextView({crust})
+  }
 
   return (
     <section className="PizzaCrust">
       <h2 className="PizzaCrust-title">Select your pizza crust:</h2>
       <div className="container">
         <div className="PizzaCrust-container row">
-          <button className="PizzaCrust-icon col-5" onClick={onNextView}>
-            <PizzaThinIcon className="pizza-icon col-12"/>
-            <span>Thin - (+$2)</span>
-          </button>
-          <button className="PizzaCrust-icon col-5" onClick={onNextView}>
-            <PizzaThickIcon className="pizza-icon col-12"/>
-            <span>Thick - (+$4)</span>
-          </button>
+          {
+            crusts.map((item: Crust, index: number) => {
+              return (
+                <button key={`pizza-crust--${index}`}  className="PizzaCrust-icon col-5" onClick={() => setCrust(item)}>
+                  {
+                    item.name.toLowerCase() === 'thin'
+                    ? <PizzaThinIcon className="pizza-icon col-12"/>
+                    : <PizzaThickIcon className="pizza-icon col-12"/>
+                  }
+                  <span>{item.name} - (+${item.price})</span>
+                </button>
+              )
+            })
+          }
         </div>
       </div>
     </section>
