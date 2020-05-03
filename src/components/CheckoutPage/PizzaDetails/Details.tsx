467ce97273a50f4062ecc5797778b0e9d7cc7256
makus <markkennethortiz@gmail.com>
2020-05-04 00:42:16 +0800

    chore: display total amount

--- src/components/CheckoutPage/PizzaDetails/Details.tsx
+++ src/components/CheckoutPage/PizzaDetails/Details.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './Details.css'
 
 function Details(props: any) {
-  const { cart, onNextView } = props
-  const { size, crust, toppings } = cart
+  const { cart, onNextView,  addOns } = props
+  const { size, crust, toppings, totalPrice } = cart
+  const { freeToppings, excessToppingsPrice } = addOns
+  const formatAmount = (amount: number) => `(+$${amount.toFixed(2)})`
 
   return (
     <main className="Details container">
@@ -12,24 +14,28 @@ function Details(props: any) {
         <div className="Details-custom-pizza row">
           <div className="Details-custom-pizza--col col-md-4 col-12">
             <h4 className="Details-title-3">Size</h4>
-            <p>{size.name}</p>
+            <p>{size.name} {formatAmount(size.price)}</p>
           </div>
           <div className="Details-custom-pizza--col col-md-4 col-12">
             <h4 className="Details-title-3">Crust</h4>
-            <p>{crust.name}</p>
+            <p>{crust.name} {formatAmount(crust.price)}</p>
           </div>
           <div className="Details-custom-pizza--col col-md-4 col-12">
             <h4 className="Details-title-3">Toppings</h4>
             <ul>
               {
-                toppings.map((item:string) => <li>{item}</li>)
+                toppings.map((item:string, index: number) => (
+                  <li key={`toppings-${item}`}>
+                    {item} {index >= freeToppings && formatAmount(excessToppingsPrice)}
+                  </li>
+                ))
               }
             </ul>
           </div>
         </div>
         <div className="Details-custom-price row">
           <div className="col-4"><h4 className="price-label">Total Price:</h4></div>
-          <div className="col-8"><h4 className="price-amount">$123.50</h4></div>
+          <div className="col-8"><h4 className="price-amount">${totalPrice.toFixed(2)}</h4></div>
         </div>
       </div>
       <button className="Details-button" onClick={onNextView}>Bake it now!</button>
