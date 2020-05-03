d03bcce68beb69ea0961ae83d23b3dfc3b92df22
makus <markkennethortiz@gmail.com>
2020-05-03 15:21:44 +0800

    chore: add size to cart

--- src/components/OrderPage/PizzaSize/Size.tsx
+++ src/components/OrderPage/PizzaSize/Size.tsx
@@ -2,27 +2,35 @@ import React from 'react'
 import './Size.css'
 import { ReactComponent as PizzaIcon } from '../../../assets/img/pizza.svg'
 
-function PizzaSize(props: any) {
+interface Sizes {
+  name: string;
+  price: number;
+  maxToppings: string;
+}
 
-  const { onNextView } = props;
+function PizzaSize(props: any) {
+  const { onNextView, sizes } = props
+  const iconSizes = ['col-6', 'col-8', 'col-12'] // Let's store this to array for simplicity
+
+  const setSize = (size: Sizes) => {
+    onNextView({size})
+  }
 
   return (
     <section className="PizzaSize">
       <h2 className="PizzaSize-title">Select your pizza size:</h2>
       <div className="container">
         <div className="PizzaSize-choices row">
-          <button className="PizzaSize-icon col-md-3 col-8" onClick={onNextView}>
-            <PizzaIcon className="pizza-icon col-6"/>
-            <span>Small - $5</span>
-          </button>
-          <button className="PizzaSize-icon col-md-3 col-8" onClick={onNextView}>
-            <PizzaIcon className="pizza-icon col-8"/>
-            <span>Medium - $7</span>
-          </button>
-          <button className="PizzaSize-icon col-md-3 col-8" onClick={onNextView}>
-            <PizzaIcon className="pizza-icon col-12"/>
-            <span>Large - $9</span>
-          </button>
+          {
+            sizes.map((item: Sizes, index: number) => {
+              const { name, price } = item
+              return (
+              <button key={`pizza-icon--${index}`} className="PizzaSize-icon col-md-3 col-8" onClick={() => setSize(item)}>
+                <PizzaIcon className={`pizza-icon ${iconSizes[index]}`}/>
+                <span>{name} - ${price}</span>
+              </button>
+            )})
+          }
         </div>
       </div>
     </section>
